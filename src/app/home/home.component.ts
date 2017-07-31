7f87ac737cb875d890844806647bbadd6bb07a41
ManfredSteyer <manfred.steyer@gmx.net>
2017-07-31 22:12:48 +0200

    info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
     template: `
-        <h1>Willkommen!</h1>    
+        <h1>Willkommen!</h1>  
+        <p>
+            
+        </p>  
     `
 })
-
 export class HomeComponent implements OnInit {
     constructor() { }
 
